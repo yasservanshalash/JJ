affbd86f70b9945c202ba1a18bce6293f51d228a
Yasser Shalash <yasservanshalash@gmail.com>
2023-05-30 15:35:13 +0200

    added css animation navbar

--- src/components/icons/SettingsIcon.tsx
+++ src/components/icons/SettingsIcon.tsx
@@ -1,13 +1,13 @@
 'use client';
 import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
-
+import styles from "../layout/navbar/NavBar.module.css"
 
 const SettingsIco = () => {
   return (
     <>
       <IconButton>
-        <SettingsIcon sx={{color: "#637381"}}/>
+        <SettingsIcon sx={{color: "#637381"}} className={styles.spin}/>
       </IconButton>
     </>
   );

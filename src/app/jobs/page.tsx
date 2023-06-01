bbe4f45f0742a317765c22496117c130d7a3c228
Yasser Shalash <yasservanshalash@gmail.com>
2023-06-01 21:20:07 +0200

    added job type filter and salary filter

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,7 +1,9 @@
 import JobCard from "@/components/cards/JobCard";
 import CategoryInput from "@/components/inputs/jobs/Category";
+import JobTypeSelect from "@/components/inputs/jobs/JobTypeSelect";
 import KeywordInput from "@/components/inputs/jobs/Keyword";
 import LocationInput from "@/components/inputs/jobs/Location";
+import SalaryRange from "@/components/inputs/jobs/SalaryRange";
 import SearchJobsButton from "@/components/inputs/jobs/SearchButton";
 import SearchForms from "@/components/inputs/jobs/SearchForms";
 
@@ -9,6 +11,10 @@ const page = () => {
   return (
     <main className="lg:w-[1200px] m-auto">
       <SearchForms/>
+      <section className="flex">
+      <JobTypeSelect />
+      <SalaryRange />
+      </section>
       <section className="flex flex-wrap md:grid md:grid-cols-2 lg:w-[1200px] lg:m-auto lg:grid lg:grid-cols-3 items-center">
       <JobCard />
       <JobCard />

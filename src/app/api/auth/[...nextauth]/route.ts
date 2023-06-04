fd500f83fc42ffb3e017ee035137231fd02c8a39
Yasser Shalash <yasservanshalash@gmail.com>
2023-06-04 20:53:51 +0200

    testing google

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "../../../../../lib/prisma";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -11,6 +12,10 @@ const handler = NextAuth({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    })
   ],
 });
 export { handler as GET, handler as POST };

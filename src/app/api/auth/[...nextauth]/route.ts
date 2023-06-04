72425fac634f3a65689af71f55dad7c40c263b51
Yasser Shalash <yasservanshalash@gmail.com>
2023-06-04 19:52:57 +0200

    hopefully done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,16 @@
-import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import { prisma } from '../../../../../lib/prisma';
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
+import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { prisma } from "../../../../../lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-  ]
-};
-
-const handler = NextAuth(authOptions);
+  ],
+});
 export { handler as GET, handler as POST };
